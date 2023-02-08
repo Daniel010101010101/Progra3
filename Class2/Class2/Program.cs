0915a6fc7c78d8ca0df19cb6f1a19a0b4281a5eb
Your NameDaniel010101010101 <danieltejada683@gmail.com>
2023-02-08 13:30:38 -0600

    Class2

---
+++ Class2/Class2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        class Persona
+        {
+            public string nombre;
+            public int edad;
+            public double peso;
+            public double altura;
+
+            void Comer()
+            {
+                Console.WriteLine("Puedo Comer");
+            }
+
+            void Caminar()
+            {
+                Console.WriteLine("Puedo Caminar");
+            }
+        }
+
+        
+    }
+}
