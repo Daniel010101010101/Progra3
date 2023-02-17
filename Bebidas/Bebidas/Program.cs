fd9006f18a9653f6595b198e7c4d4fd48bde1ed3
Daniel010101010101 <88622971+Daniel010101010101@users.noreply.github.com>
2023-02-17 16:21:07 -0600

    Bebidas

--- Bebidas/Bebidas/Program.cs
+++ Bebidas/Bebidas/Program.cs
@@ -1,46 +1,232 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using static System.Console;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Bebidas
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Bebida[] bebida = new Bebida[5];
+            Venta venta = new Venta();
+            venta.Compra();
 
-            bebida[0] = new Venta("Soda" + "A mano" + "Alcohol" + 12 + " " + " ");
         }
     }
 }
+
 public abstract class Bebida
 {
-    public string NombreBebida { get; set; }
-    public string Preparacion { get; set; }
-    public string NombrePersona { get; set; }
-    public int edad { get; set; }
-    public string Categoria { get; set; }
-    public string empleado { get; set; }
-
-    public abstract string Vender();
+    public string nombreBebida { get; set; }
+    public string tipo { get; set; }
+    public string categoria { get; set; }
+    public string preparacion { get; set; }
+    public string nombreCliente { get; set; }
+    public int  edad { get; set; }
+    public string  empleado { get; set; }
+    
 }
 
 public class Venta : Bebida
 {
-    
-    public  Venta(string nombreBedida, string preparacion, string nombrepersona, int edad, string categoria, string empleado)
+    public Venta(string nombreBebida, string tipo, string categoria, string nombreCliente, int edad, string empleado, string preparacion)
     {
-        this.NombreBebida = nombreBedida;
-        this.Preparacion = preparacion;
-        this.Categoria = categoria;
-    }
-    public override string Vender()
-    {
-        string dato = "Bienvenido " + this.NombrePersona;
-        return dato;
+        this.nombreBebida = nombreBebida;
+        this.tipo = tipo;
+        this.categoria = categoria;
+        this.nombreCliente = nombreCliente;
+        this.edad = edad;
+        this.empleado = empleado;
+        this.preparacion = preparacion;
     }
 
+    public Venta()
+    {
+    }
+
+    public void Compra()
+    {
+        int opc;
+        do
+        {
+            WriteLine("Bienvenido");
+            WriteLine("Selecciona una opcion");
+            WriteLine("1. Café \t 2. Soda \t 3. Cerveza \t 4. Tequila");
+            opc = int.Parse(ReadLine());
+            if (opc == 1)
+            { 
+                Clear();
+                WriteLine("Haz seleccionado Café"); 
+                WriteLine("\nIngresa tu nombre: ");
+                nombreCliente = ReadLine();
+                WriteLine("\nIngresa tu edad: ");
+                edad = int.Parse(ReadLine());
+                Clear();
+                WriteLine("****Hora de realizar tu orden****");
+                empleado = "Jesse Pinkman";
+                WriteLine("\nEstas siendo atendido por: " + empleado);
+                WriteLine("\n****Haz comprado un Café****");
+                nombreBebida = "Café";
+                categoria = "Cafeina";
+                tipo = "Café negro";
+                preparacion = "Herver";
+                WriteLine("\n****Factura****" +
+                          "\nNombre del cliente: " + nombreCliente +
+                          "\nEdad del cliente: " + edad +
+                          "\nNombre de la bebida: " + nombreBebida + 
+                          "\nCategoría: " + categoria + 
+                          "\nTipo: " + tipo +
+                          "\nPreparación: "+ preparacion +
+                          "\nTOTAL: 3.50 $$$$");
+                WriteLine("\n1. Continuar \t 2. Volver al menu principal");
+                int opc2 = int.Parse(ReadLine());
+                if (opc2 == 1)
+                {
+                    WriteLine("Tu compra fue realizada con exito, Muchas gracias!!!");
+                }
+                else
+                {
+                    Clear();
+                    Compra();
+                }
+                
+                break;
+                
+
+            }
+            if (opc == 2)
+            {
+                Clear();
+                WriteLine("Haz seleccionado Soda"); 
+                WriteLine("\nIngresa tu nombre: ");
+                nombreCliente = ReadLine();
+                WriteLine("\nIngresa tu edad: ");
+                edad = int.Parse(ReadLine());
+                Clear();
+                WriteLine("****Hora de realizar tu orden****");
+                empleado = "Phillip Brown";
+                WriteLine("\nEstas siendo atendido por: " + empleado);
+                WriteLine("\n****Haz comprado una Soda****");
+                nombreBebida = "Coca-Cola";
+                categoria = "Soda";
+                tipo = "Gaseosa";
+                preparacion = "No se";
+                WriteLine("\n****Factura****" +
+                          "\nNombre del cliente: " + nombreCliente +
+                          "\nEdad del cliente: " + edad +
+                          "\nNombre de la bebida: " + nombreBebida + 
+                          "\nCategoría: " + categoria + 
+                          "\nTipo: " + tipo +
+                          "\nPreparación: "+ preparacion +
+                          "\nTOTAL: 1.50 $$$$");
+                WriteLine("\n1. Continuar \t 2. Volver al menu principal");
+                int opc3 = int.Parse(ReadLine());
+                if (opc3 == 1)
+                {
+                    WriteLine("Tu compra fue realizada con exito, Muchas gracias!!!");
+                }
+                else
+                {
+                    Clear();
+                    Compra();
+                }
+                break;
+            }
+            if (opc == 3)
+            {
+                Clear();
+                WriteLine("Haz seleccionado Cerveza"); 
+                WriteLine("\nIngresa tu nombre: ");
+                nombreCliente = ReadLine();
+                WriteLine("\nIngresa tu edad: ");
+                edad = int.Parse(ReadLine());
+                if (edad < 18)
+                {
+                    WriteLine("No puedes comprar bebidas embriagantes porque eres menor  de edad");
+                }
+                else
+                {
+                    Clear();
+                    WriteLine("****Hora de realizar tu orden****");
+                    empleado = "Sr. White";
+                    WriteLine("\nEstas siendo atendido por: " + empleado);
+                    WriteLine("\n****Haz comprado una Cerveza****");
+                    nombreBebida = "Pilsener";
+                    categoria = "Alcohol";
+                    tipo = "Cerveza";
+                    preparacion = "En lata";
+                    WriteLine("\n****Factura****" +
+                              "\nNombre del cliente: " + nombreCliente +
+                              "\nEdad del cliente: " + edad +
+                              "\nNombre de la bebida: " + nombreBebida + 
+                              "\nCategoría: " + categoria + 
+                              "\nTipo: " + tipo +
+                              "\nPreparación: "+ preparacion +
+                              "\nTOTAL: 2.50 $$$$");
+                    WriteLine("\n1. Continuar \t 2. Volver al menu principal");
+                    int opc3 = int.Parse(ReadLine());
+                    if (opc3 == 1)
+                    {
+                        WriteLine("Tu compra fue realizada con exito, Muchas gracias!!!");
+                    }
+                    else
+                    {
+                        Clear();
+                        Compra();
+                    }
+                }
+                break;
+            }
+            if (opc == 4)
+            {
+                Clear();
+                WriteLine("Haz seleccionado Tequila"); 
+                WriteLine("\nIngresa tu nombre: ");
+                nombreCliente = ReadLine();
+                WriteLine("\nIngresa tu edad: ");
+                edad = int.Parse(ReadLine());
+                if (edad < 18)
+                {
+                    WriteLine("No puedes comprar bebidas embriagantes porque eres menor  de edad");
+                }
+                else
+                {
+                    Clear();
+                    WriteLine("****Hora de realizar tu orden****");
+                    empleado = "Saul Goodman";
+                    WriteLine("\nEstas siendo atendido por: " + empleado);
+                    WriteLine("\n****Haz comprado Tequila****");
+                    nombreBebida = "Vodka";
+                    categoria = "Alcohol";
+                    tipo = "Tequila";
+                    preparacion = "Conservación";
+                    WriteLine("\n****Factura****" +
+                              "\nNombre del cliente: " + nombreCliente +
+                              "\nEdad del cliente: " + edad +
+                              "\nNombre de la bebida: " + nombreBebida + 
+                              "\nCategoría: " + categoria + 
+                              "\nTipo: " + tipo +
+                              "\nPreparación: "+ preparacion +
+                              "\nTOTAL: 20.00 $$$$");
+                    WriteLine("\n1. Continuar \t 2. Volver al menu principal");
+                    int opc3 = int.Parse(ReadLine());
+                    if (opc3 == 1)
+                    {
+                        WriteLine("Tu compra fue realizada con exito, Muchas gracias!!!");
+                    }
+                    else
+                    {
+                        Clear();
+                        Compra();
+                    }
+                }
+                break;
+            }
+            
+        } while (opc <= 4);
+    }
+    
 }
